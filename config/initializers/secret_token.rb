e6c42e7a50c502d08d9c120573f9e77c28a22b41
Raghu Betina <rbetina@gmail.com>
2013-04-15 14:47:25 -0500

    rails new yearbook_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YearbookApp::Application.config.secret_token = '1452ddbf579720b7d1af445fc7fd132f378099632897f2845b99f43977907da75a7124cc4499bce582ec3bd38a6971ca4a47289c3bbf8a3c9e41218da6aeef4a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+YearbookApp::Application.config.session_store :cookie_store, key: '_yearbook_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# YearbookApp::Application.config.session_store :active_record_store
